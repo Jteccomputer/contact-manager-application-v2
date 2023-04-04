f305045708c578aa4381abc1fe01867be1c8a1f1
Jteccomputer <chukwukachinedujohn@gmail.com>
2023-04-04 12:58:36 +0100

    created server, created users route and controllers

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const usersRoute = require("./routes/usersRoute");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// we are going to create a new middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", usersRoute);
+
+// building default route for our home page
+app.get('/', (req, res) => {
+
+    res.send('home page')
+});
+
+app.listen(PORT, () =>  {
+    console.log(`Server listening on port ${PORT}!`);
+});
